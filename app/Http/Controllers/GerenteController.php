7d0a46c864ef0083ba4b2387377fbe999d44c163
Franco Sanguinetti <fsanguinetti05@gmail.com>
2023-10-30 01:09:29 -0300

    Eliminar, editar y listar paquete. Crear, editar listar y eliminar lote, asignar paquete a lote

--- app/Http/Controllers/GerenteController.php
+++ app/Http/Controllers/GerenteController.php
@@ -7,6 +7,12 @@ use Illuminate\Support\Facades\Validator;
 use App\Models\Paquete;
 use App\Models\PaqueteEstante;
 use App\Models\Estante;
+use App\Models\Almacen;
+use App\Models\Lote;
+use App\Models\Forma;
+use App\Models\EstadoF;
+use App\Models\LoteCamion;
+use App\Models\CamionLlevaLote;
 
 class GerenteController extends Controller
 {
@@ -15,7 +21,7 @@ class GerenteController extends Controller
         $apiKey = '7a6TfdGhaJbpPMG2ehCfSExHYsnzdkIb5a0YlJzjU5U';
         $address = urlencode($direccion);
         $countryName = "Uruguay";
-        $url = "https://geocode.search.hereapi.com/v1/geocode?q=$address&apiKey=$apiKey&country=$countryName";
+        $url = "https://geocode.search.hereapi.com/v1/geocode?q=$address&country=$countryName&apiKey=$apiKey";
 
         $response = @file_get_contents($url);
         $data = json_decode($response);
@@ -34,10 +40,8 @@ class GerenteController extends Controller
             ) {
                 return true;
             } else {
-                return response()->json(['error' => 'Dirección incompleta'], 400);
+                return response()->json(['error' => 'Dirección inválida'], 400);
             }
-        } else {
-            return response()->json(['error' => 'Dirección inválida'], 400);
         }
     }
 
@@ -75,17 +79,60 @@ class GerenteController extends Controller
         return response()->json(['success' => 'Paquete creado, código: ' . $validatedData['Codigo']], 200);
     }
 
-    public function listarPaquetes()
+    public function eliminarPaquete($id)
     {
-        $paquetesEnEstante = PaqueteEstante::whereNull('deleted_at')->get();
-        $paquetes = Paquete::whereNotIn('ID', $paquetesEnEstante->pluck('ID_Paquete'));
-
-        $paquetesCombinados = $paquetes->union($paquetesEnEstante)->get();
-
-        return response()->json(['success' => $paquetesCombinados], 200);
+        $paquete = Paquete::findOrFail($id);
+        Forma::where('ID_Paquete', $id)->delete();
+        PaqueteEstante::where('ID_Paquete', $id)->delete();
+        $paquete->delete();
+        return response()->json(['success' => 'Paquete con codigo ' . $paquete->Codigo . ' eliminado'], 200);
     }
 
+    public function editarPaquete(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ID' => 'required|exists:paquete,ID',
+            'Descripcion' => 'string',
+            'Peso_Kg' => 'numeric|min:1',
+            'ID_Cliente' => 'exists:cliente,ID',
+            'ID_Estado' => 'exists:estadop,ID',
+            'Calle' => 'string',
+            'Numero_Puerta' => 'string',
+            'Ciudad' => 'string',
+        ]);
 
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 400);
+        }
+
+        $validatedData = $validator->validated();
+
+        $paquete = Paquete::findOrFail($validatedData['ID']);
+
+        if (isset($validatedData['Calle']) || isset($validatedData['Numero_Puerta']) || isset($validatedData['Ciudad'])) {
+            $direccion = $validatedData['Calle'] . ' ' . $validatedData['Numero_Puerta'] . ', ' . $validatedData['Ciudad'] . ', ' . 'Uruguay';
+
+            $direccionValida = $this->validarDireccion($direccion);
+
+            if ($direccionValida !== true) {
+                return $direccionValida;
+            }
+
+            $validatedData['Destino'] = $direccion;
+        }
+
+        $paquete->update($validatedData);
+
+        return response()->json(['success' => 'Paquete editado'], 200);
+    }
+
+    public function listarPaquetesAlmacen($id)
+    {
+        $almacen = Almacen::findOrFail($id);
+        $paquetesEnAlmacen = PaqueteEstante::where('ID_Almacen', $id)->whereNull('deleted_at')->get();
+
+        return response()->json(['Paquetes' => $paquetesEnAlmacen], 200);
+    }
 
     public function buscarPaquete(Request $request)
     {
@@ -199,4 +246,92 @@ class GerenteController extends Controller
 
         return response()->json(['success' => 'Paquete quitado de estante'], 200);
     }
+
+    public function crearLote(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'Descripcion' => 'string',
+            'Peso_Kg' => 'numeric|required|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $lote = Lote::create($validatedData);
+
+        return response()->json(['success' => 'Lote creado con ID ' . $lote->ID], 200);
+    }
+
+    public function listarLotes()
+    {
+        $lotes = Lote::all();
+        return response()->json(['Lotes' => $lotes], 200);
+    }
+
+    public function asignarPaqueteLote(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ID_Paquete' => 'required|exists:paquete,ID',
+            'ID_Lote' => 'required|exists:lote,ID',
+            'ID_Estado' => 'required|exists:estadof,ID',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        try {
+            $paquete = Paquete::where('ID', $validatedData['ID_Paquete'])->whereNull('deleted_at')->firstOrFail();
+            $lote = Lote::where('ID', $validatedData['ID_Lote'])->whereNull('deleted_at')->firstOrFail();
+            $estado = EstadoF::where('ID', $validatedData['ID_Estado'])->whereNull('deleted_at')->firstOrFail();
+        } catch (\Exception $e) {
+            return response()->json(['error' => 'Paquete, lote o estado no encontrado'], 404);
+        }
+
+
+        Forma::create([
+            'ID_Lote' => $validatedData['ID_Lote'],
+            'ID_Paquete' => $validatedData['ID_Paquete'],
+            'ID_Estado' => $validatedData['ID_Estado'],
+        ]);
+
+        return response()->json(['success' => 'Paquete ' . $paquete->ID . ' asignado a lote ' . $lote->ID . ' con estado ' . $estado->Estado], 200);
+    }
+
+    public function editarLote(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ID' => 'required|exists:lote,ID',
+            'Descripcion' => 'string',
+            'Peso_Kg' => 'numeric|min:1',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $lote = Lote::findOrFail($validatedData['ID']);
+
+        $lote->update($validatedData);
+
+        return response()->json(['success' => 'Lote ' . $lote->ID . ' editado'], 200);
+    }
+
+    public function eliminarLote($id)
+    {
+        $lote = Lote::findOrFail($id);
+        Forma::where('ID_Lote', $id)->delete();
+        CamionLlevaLote::where('ID_Lote', $id)->delete();
+        LoteCamion::where('ID_Lote', $id)->delete();
+        $lote->delete();
+        return response()->json(['success' => 'Lote ' . $lote->ID . ' eliminado'], 200);
+    }
+
 }
