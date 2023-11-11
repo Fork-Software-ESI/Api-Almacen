a8890723c462445d03b7121fa335a5f2a0a98227
Franco Sanguinetti <fsanguinetti05@gmail.com>
2023-11-11 05:27:14 -0300

    Agrega modelos y métodos para gestionar conductores y camiones, y actualiza los métodos existentes para gestionar paquetes y lotes.

--- app/Http/Controllers/GerenteController.php
+++ app/Http/Controllers/GerenteController.php
@@ -13,6 +13,13 @@ use App\Models\Forma;
 use App\Models\EstadoF;
 use App\Models\LoteCamion;
 use App\Models\CamionLlevaLote;
+use App\Models\Chofer;
+use App\Models\Camion;
+use App\Models\ChoferCamion;
+use App\Models\CamionPlataforma;
+use App\Models\CamionPlataformaSalida;
+use App\Models\Plataforma;
+
 
 class GerenteController extends Controller
 {
@@ -73,6 +80,9 @@ class GerenteController extends Controller
 
         $validatedData['Destino'] = $direccion;
         $validatedData['Codigo'] = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);
+        if (Paquete::where('Codigo', $validatedData['Codigo'])->first() !== null) {
+            $validatedData['Codigo'] = substr(str_shuffle("0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"), 0, 6);
+        }
 
         Paquete::create($validatedData);
 
@@ -129,7 +139,7 @@ class GerenteController extends Controller
     public function listarPaquetesAlmacen($id)
     {
         $almacen = Almacen::findOrFail($id);
-        $paquetesEnAlmacen = PaqueteEstante::where('ID_Almacen', $id)->whereNull('deleted_at')->get();
+        $paquetesEnAlmacen = PaqueteEstante::where('ID_Almacen', $almacen->ID)->whereNull('deleted_at')->get();
 
         return response()->json(['Paquetes' => $paquetesEnAlmacen], 200);
     }
@@ -265,7 +275,7 @@ class GerenteController extends Controller
         return response()->json(['success' => 'Lote creado con ID ' . $lote->ID], 200);
     }
 
-    public function listarLotes()
+    public function listarLotes(Request $request)
     {
         $lotes = Lote::all();
         return response()->json(['Lotes' => $lotes], 200);
@@ -293,6 +303,13 @@ class GerenteController extends Controller
             return response()->json(['error' => 'Paquete, lote o estado no encontrado'], 404);
         }
 
+        if (Forma::where('ID_Paquete', $validatedData['ID_Paquete'])->whereNull('deleted_at')->first() !== null) {
+            return response()->json(['error' => 'Paquete ya asignado a un lote'], 422);
+        }
+
+        if ($lote->ID_Estado != 1) {
+            return response()->json(['error' => 'Lote no disponible'], 422);
+        }
 
         Forma::create([
             'ID_Lote' => $validatedData['ID_Lote'],
@@ -334,4 +351,154 @@ class GerenteController extends Controller
         return response()->json(['success' => 'Lote ' . $lote->ID . ' eliminado'], 200);
     }
 
+    public function verChoferes()
+    {
+        $choferes = Chofer::whereNull('deleted_at')->get();
+        return response()->json(['Choferes' => $choferes], 200);
+    }
+
+    public function verCamiones()
+    {
+        $camiones = Camion::whereNull('deleted_at')->get();
+        return response()->json(['Camiones' => $camiones], 200);
+    }
+
+    public function verCamionesSinChofer()
+    {
+        $camiones = Camion::whereNull('camion.deleted_at')
+            ->where(function ($query) {
+                $query->whereDoesntHave('chofers', function ($subquery) {
+                    $subquery->whereNull('chofer.deleted_at');
+                })->orWhereHas('chofers', function ($subquery) {
+                    $subquery->whereNotNull('chofer_camion.deleted_at');
+                })->orWhereHas('chofers', function ($subquery) {
+                    $subquery->whereNull('chofer_camion.deleted_at')
+                        ->where('chofer_camion.ID_Estado', 5);
+                });
+            })
+            ->get();
+
+        return response()->json(['Camiones' => $camiones], 200);
+    }
+
+    public function verCamionesDisponibles()
+    {
+        $response = $this->verCamionesSinChofer();
+        $camionesSinChoferData = json_decode($response->getContent());
+
+        $camionesSinChoferIds = collect($camionesSinChoferData->Camiones)->pluck('ID');
+        $camionesDisponibles = Camion::whereNull('camion.deleted_at')
+            ->whereNotIn('ID', $camionesSinChoferIds)
+            ->get();
+
+        $camionesFinales = ChoferCamion::whereIn('ID_Camion', $camionesDisponibles->pluck('ID')->toArray())
+            ->whereNull('deleted_at')
+            ->where('ID_Estado', 1)
+            ->get();
+
+        return response()->json(['Camiones disponibles' => $camionesFinales], 200);
+    }
+
+    public function verChoferesDisponibles()
+    {
+        $choferes = Chofer::whereNull('chofer.deleted_at')
+            ->where(function ($query) {
+                $query->whereDoesntHave('camions', function ($subquery) {
+                    $subquery->whereNull('camion.deleted_at');
+                })->orWhereHas('camions', function ($subquery) {
+                    $subquery->whereNotNull('chofer_camion.deleted_at');
+                })->orWhereHas('camions', function ($subquery) {
+                    $subquery->whereNull('chofer_camion.deleted_at')
+                        ->where('chofer_camion.ID_Estado', 5);
+                });
+            })
+            ->get();
+
+        return response()->json(['Choferes' => $choferes], 200);
+    }
+
+    public function asignarChoferCamion(Request $request)
+    {
+        $data = $request->validate([
+            'ID_Chofer' => 'required|exists:chofer,ID',
+            'ID_Camion' => 'required|exists:camion,ID',
+        ]);
+
+        $choferesDisponiblesResponse = $this->verChoferesDisponibles();
+        $choferesDisponiblesData = json_decode($choferesDisponiblesResponse->getContent());
+
+        $camionesSinChoferResponse = $this->verCamionesSinChofer();
+        $camionesSinChoferData = json_decode($camionesSinChoferResponse->getContent());
+
+        $choferesDisponibles = collect($choferesDisponiblesData->Choferes);
+        if (!$choferesDisponibles->contains('ID', $data['ID_Chofer'])) {
+            return response()->json(['error' => 'El chofer no está disponible'], 400);
+        }
+
+        $camionesSinChofer = collect($camionesSinChoferData->Camiones);
+        if (!$camionesSinChofer->contains('ID', $data['ID_Camion'])) {
+            return response()->json(['error' => 'El camión no está disponible'], 400);
+        }
+
+        $choferCamion = ChoferCamion::create([
+            'ID_Chofer' => $data['ID_Chofer'],
+            'ID_Camion' => $data['ID_Camion'],
+            'Fecha_Hora_Inicio' => now(),
+            'ID_Estado' => 1,
+        ]);
+
+        return response()->json(['message' => "Chofer ID: " . $choferCamion->ID_Chofer . " asignado a Camión ID: " . $choferCamion->ID_Camion . " con Estado: " . $choferCamion->ID_Estado], 200);
+    }
+
+    public function listarChoferesCamiones()
+    {
+        $choferesCamiones = ChoferCamion::whereNull('deleted_at')->where('ID_Estado', '<>', 5)->get();
+        return response()->json(['ChoferesCamiones' => $choferesCamiones], 200);
+    }
+
+    public function verCamionesEnPlataformas(Request $request)
+    {
+        $almacen = Almacen::findOrFail($request->ID_Almacen);
+        $camionesEnPlataformas = CamionPlataforma::select('camion_plataforma.ID_Camion', 'camion.Matricula', 'camion_plataforma.Numero_Plataforma', 'camion_plataforma.Fecha_Hora_Llegada')
+            ->leftJoin('camion_plataforma_salida', function ($join) {
+                $join->on('camion_plataforma.ID_Camion', '=', 'camion_plataforma_salida.ID_Camion')
+                    ->on('camion_plataforma.ID_Almacen', '=', 'camion_plataforma_salida.ID_Almacen')
+                    ->on('camion_plataforma.Numero_Plataforma', '=', 'camion_plataforma_salida.Numero_Plataforma');
+            })
+            ->leftJoin('camion', 'camion_plataforma.ID_Camion', '=', 'camion.ID')
+            ->where('camion_plataforma.ID_Almacen', $almacen->ID)
+            ->whereNull('camion_plataforma.deleted_at')
+            ->where(function ($query) {
+                $query->whereNull('camion_plataforma_salida.Fecha_Hora_Salida')
+                    ->orWhereNotNull('camion_plataforma_salida.deleted_at');
+            })
+            ->get();
+
+        return $camionesEnPlataformas;
+    }
+
+    public function verCamionesEnTransito()
+    {
+        $camionesEnTransito = ChoferCamion::where('ID_Estado', 4)->whereNull('deleted_at')->get();
+        if (!$camionesEnTransito) {
+            return response()->json(['error' => 'No hay camiones en tránsito'], 404);
+        }
+
+        // Crear un array vacío para almacenar los horarios de salida
+        $horariosSalida = [];
+
+        // Iterar sobre cada camión en tránsito
+        foreach ($camionesEnTransito as $camion) {
+            // Obtener el horario de salida del camión
+            $horarioSalida = CamionPlataformaSalida::where('ID_Camion', $camion->ID_Camion)->value('Fecha_Hora_Salida');
+            // Agregar el horario de salida al array horariosSalida
+            $horariosSalida[$camion->ID_Camion] = $horarioSalida;
+        }
+
+        // Retornar el array horariosSalida junto con la lista de camiones en tránsito
+        return response()->json([
+            'Camiones en tránsito' => $camionesEnTransito,
+            'Horarios de salida' => $horariosSalida
+        ], 200);
+    }
 }
