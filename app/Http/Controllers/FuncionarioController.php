7b76d7f204f8c2bb1210d2c5efa3434c09f6535d
Franco Sanguinetti <fsanguinetti05@gmail.com>
2023-11-13 20:10:46 -0300

    Add models and methods for managing paquetes in estantes and lotes. Matenme porfa

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -9,7 +9,10 @@ use App\Models\PaqueteEstante;
 use App\Models\Estante;
 use App\Models\FuncionarioPaqueteEstante;
 use App\Models\Lote;
-
+use App\Models\GerenteAlmacen;
+use App\Models\FuncionarioAlmacen;
+use App\Models\GerenteForma;
+use App\Models\Forma;
 
 class FuncionarioController extends Controller
 {
@@ -19,14 +22,20 @@ class FuncionarioController extends Controller
             'ID_Funcionario' => 'required|exists:funcionario_almacen,ID',
             'ID_Paquete' => 'required|exists:paquete,ID',
             'ID_Estante' => 'required|exists:estante,ID',
-            'ID_Almacen' => 'required|exists:estante,ID_Almacen',   
+            'ID_Almacen' => 'required|exists:estante,ID_Almacen',
         ]);
-    
+
         $paquete = Paquete::findOrFail($validatedData['ID_Paquete']);
-        $estante = Estante::where('ID', $validatedData['ID_Estante'])
+        $estante = Estante::where('ID', $validatedData['ID_Estante'])->where('ID_Almacen', $validatedData['ID_Almacen'])->firstOrFail();
+
+        $paqueteExistente = PaqueteEstante::where('ID_Paquete', $validatedData['ID_Paquete'])
             ->where('ID_Almacen', $validatedData['ID_Almacen'])
-            ->firstOrFail();
-    
+            ->first();
+
+        if ($paqueteExistente) {
+            return response()->json(['error' => 'Paquete ya se encuentra en un estante en el mismo almacén'], 422);
+        }
+
         $paqueteEstante = PaqueteEstante::firstOrCreate([
             'ID_Paquete' => $validatedData['ID_Paquete'],
             'ID_Estante' => $validatedData['ID_Estante'],
@@ -37,9 +46,9 @@ class FuncionarioController extends Controller
             'ID_Funcionario' => $validatedData['ID_Funcionario'],
             'ID_Paquete' => $validatedData['ID_Paquete'],
         ]);
-    
+
         if ($paqueteEstante->wasRecentlyCreated) {
-            return response()->json(['success' => 'Paquete ' . $paquete->Codigo . ' registrado en estante con ID ' . $paqueteEstante->ID_Estante . ' en almacén ' . $paqueteEstante->ID_Almacen], 200);
+            return response()->json(['success' => 'Paquete ' . $paquete->Codigo . ' registrado en estante con ID ' . $estante->ID . ' en almacén ' . $paqueteEstante->ID_Almacen], 200);
         } else {
             return response()->json(['error' => 'Paquete ya se encuentra en un estante'], 422);
         }
@@ -78,7 +87,7 @@ class FuncionarioController extends Controller
 
         $paqueteEstante->save();
 
-        return response()->json(['success' => 'Paquete trasladado a estante con ID ' . $paqueteEstante->ID_Estante . ' en almacén ' . $paqueteEstante->ID_Almacen], 200);
+        return response()->json(['success' => 'Paquete trasladado a estante con ID ' . $estante->ID . ' en almacén ' . $paqueteEstante->ID_Almacen], 200);
     }
 
     public function quitarPaqueteDeEstante(Request $request)
@@ -94,9 +103,106 @@ class FuncionarioController extends Controller
         return response()->json(['success' => 'Paquete quitado de estante'], 200);
     }
 
+    public function listarPaquetesAlmacen(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ID_Funcionario' => 'required|exists:funcionario_almacen,ID',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $funcionario = FuncionarioAlmacen::findOrFail($validatedData['ID_Funcionario']);
+
+        $validatedData = $validator->validated();
+
+        $paquetesEnAlmacen = PaqueteEstante::where('ID_Almacen', $funcionario->ID_Almacen)->whereNull('deleted_at')->get();
+
+        return response()->json(['Paquetes' => $paquetesEnAlmacen], 200);
+    }
+
     public function listarLotes(Request $request)
     {
-        $lotes = Lote::all();
-        return response()->json(['Lotes' => $lotes], 200);
+        $validator = Validator::make($request->all(), [
+            'ID_Funcionario' => 'required|exists:funcionario_almacen,ID',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $funcionario = FuncionarioAlmacen::findOrFail($validatedData['ID_Funcionario']);
+
+        $gerente = GerenteAlmacen::where('ID_Almacen', $funcionario->ID_Almacen)->firstOrFail();
+
+        $lotes = $gerente->gerente_lotes()->with('lote')->get()->pluck('lote');
+
+        return response()->json(['success' => $lotes], 200);
+    }
+
+    public function listarPaqueteLote(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ID_Funcionario' => 'required|exists:funcionario_almacen,ID',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $funcionario = FuncionarioAlmacen::findOrFail($validatedData['ID_Funcionario']);
+
+        $gerente = GerenteAlmacen::where('ID_Almacen', $funcionario->ID_Almacen)->firstOrFail();
+
+        $paquetesLotes = $gerente->gerente_formas()
+            ->whereDoesntHave('forma', function ($query) {
+                $query->whereNotNull('deleted_at');
+            })
+            ->with('forma')
+            ->get()
+            ->pluck('forma', 'ID_Paquete');
+
+        return response()->json(['success' => $paquetesLotes], 200);
+    }
+
+    public function actualizarPaqueteLote(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ID_Funcionario' => 'required|exists:funcionario_almacen,ID',
+            'ID_Paquete' => 'required|exists:forma,ID_Paquete',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $validatedData = $validator->validated();
+
+        $funcionario = FuncionarioAlmacen::where('ID', $validatedData['ID_Funcionario'])->whereNull('deleted_at')->first();
+        $paquete = Paquete::where('ID', $validatedData['ID_Paquete'])->whereNull('deleted_at')->first();
+        $paqueteLote = Forma::where('ID_Paquete', $validatedData['ID_Paquete'])->whereNull('deleted_at')->first();
+        $lote = Lote::where('ID', $paqueteLote->ID_Lote)->whereNull('deleted_at')->first();
+        $gerenteForma = GerenteForma::where('ID_Paquete', $paqueteLote->ID_Paquete)->whereNull('deleted_at')->first();
+        $gerente = GerenteAlmacen::where('ID_Gerente', $gerenteForma->ID_Gerente)->whereNull('deleted_at')->first();
+
+        if (!$funcionario || !$paquete || !$paqueteLote || !$lote || !$gerenteForma || !$gerente) {
+            return response()->json(['error' => 'Recurso no encontrado'], 404);
+        }
+
+        if ($paquete->ID_Estado != 1 || $lote->ID_Estado != 1 || $gerente->ID_Almacen != $funcionario->ID_Almacen || $paqueteLote->ID_Estado != 1) {
+            return response()->json(['error' => 'Este par paquete lote no está disponible para actualizar'], 422);
+        }
+
+        $paqueteLote->ID_Estado = 2;
+        $paqueteLote->save();
+
+        return response()->json(['success' => 'Paquete lote actualizado'], 200);
     }
 }
