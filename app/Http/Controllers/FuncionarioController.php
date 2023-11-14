e31af3f4b051f0b5415bc8fe50b647341e38021c
Franco Sanguinetti <fsanguinetti05@gmail.com>
2023-11-14 02:08:44 -0300

    Add models for LoteCamion and Camion, and implement cargarLoteCamion method in FuncionarioController

--- app/Http/Controllers/FuncionarioController.php
+++ app/Http/Controllers/FuncionarioController.php
@@ -13,6 +13,9 @@ use App\Models\GerenteAlmacen;
 use App\Models\FuncionarioAlmacen;
 use App\Models\GerenteForma;
 use App\Models\Forma;
+use App\Models\LoteCamion;
+use App\Models\Camion;
+
 
 class FuncionarioController extends Controller
 {
@@ -191,18 +194,69 @@ class FuncionarioController extends Controller
         $lote = Lote::where('ID', $paqueteLote->ID_Lote)->whereNull('deleted_at')->first();
         $gerenteForma = GerenteForma::where('ID_Paquete', $paqueteLote->ID_Paquete)->whereNull('deleted_at')->first();
         $gerente = GerenteAlmacen::where('ID_Gerente', $gerenteForma->ID_Gerente)->whereNull('deleted_at')->first();
+        $paqueteEstante = PaqueteEstante::where('ID_Paquete', $validatedData['ID_Paquete'])->whereNull('deleted_at')->first();
 
         if (!$funcionario || !$paquete || !$paqueteLote || !$lote || !$gerenteForma || !$gerente) {
             return response()->json(['error' => 'Recurso no encontrado'], 404);
+        }
+
+        if ($paqueteEstante) {
+            return response()->json(['error' => 'No se puede cargar un paquete a un lote si aún está en un estante'], 422);
         }
 
         if ($paquete->ID_Estado != 1 || $lote->ID_Estado != 1 || $gerente->ID_Almacen != $funcionario->ID_Almacen || $paqueteLote->ID_Estado != 1) {
             return response()->json(['error' => 'Este par paquete lote no está disponible para actualizar'], 422);
         }
 
+        $paquete->ID_Estado = 2;
+        $paquete->save();
         $paqueteLote->ID_Estado = 2;
         $paqueteLote->save();
 
         return response()->json(['success' => 'Paquete lote actualizado'], 200);
+    }
+
+    public function cargarLoteCamion(Request $request)
+    {
+        $validator = Validator::make($request->all(), [
+            'ID_Funcionario' => 'required|exists:funcionario_almacen,ID',
+            'ID_Lote' => 'required|exists:lote,ID',
+        ]);
+
+        if ($validator->fails()) {
+            return response()->json(['error' => $validator->errors()], 422);
+        }
+
+        $lotesAlmacen = $this->listarLotes($request)->getData()->success;
+        $validatedData = $validator->validated();
+        $funcionario = FuncionarioAlmacen::where('ID', $validatedData['ID_Funcionario'])->whereNull('deleted_at')->first();
+        $loteCamion = LoteCamion::where('ID_Lote', $validatedData['ID_Lote'])->whereNull('deleted_at')->first();
+        $lote = Lote::where('ID', $validatedData['ID_Lote'])->whereNull('deleted_at')->first();
+        $camion = Camion::where('ID', $loteCamion->ID_Camion)->whereNull('deleted_at')->first();
+        $loteForma = Forma::where('ID_Lote', $validatedData['ID_Lote'])->whereNull('deleted_at')->first();
+
+        if (!$funcionario || !$loteCamion || !$lote || !$camion || !$loteForma) {
+            return response()->json(['error' => 'Recurso no encontrado'], 404);
+        }
+
+        if ($loteCamion->ID_Estado != 1) {
+            return response()->json(['error' => 'Este lote no está disponible para actualizar'], 422);
+        }
+
+        if ($loteForma->ID_Estado != 2) {
+            return response()->json(['error' => 'Este lote no está listo para ser cargado'], 422);
+        }
+
+        foreach ($lotesAlmacen as $lote) {
+            if ($lote->ID == $validatedData['ID_Lote']) {
+                $loteCamion->ID_Estado = 2;
+
+                $loteCamion->save();
+
+                return response()->json(['success' => 'Lote ' . $loteCamion->ID_Lote . ' cargado en el camion con matricula ' . $camion->Matricula], 200);
+            }
+        }
+
+        return response()->json(['error' => 'Este lote no se encuentra en el almacen'], 422);
     }
 }
